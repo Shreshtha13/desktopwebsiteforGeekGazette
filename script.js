ae695da581af46b83b8478aeab2e2c628cd69404
Shreshtha13 <76113457+Shreshtha13@users.noreply.github.com>
2021-02-09 14:38:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector(".hamburger-menu").addEventListener("click", () => {
+    document.querySelector(".container").classList.toggle("change");
+});
+document.querySelector(".hamburger-menu").addEventListener("click", () => {
+    document.querySelector(".hamburger-menu").classList.toggle("change");
+});
